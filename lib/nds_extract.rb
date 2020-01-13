dfff790b5222c82761bab655946d7fb96eb3761c
Austin Osorio <osorio_austin@yahoo.com>
2020-01-13 04:29:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -56,7 +56,12 @@ def movies_with_director_key(name, movies_collection)
 
   while title_i < movies_collection.length do
 
-    output << {:title = movies_collection[title_i][:title]}
+    output << {
+
+      :director_name => name
+      :title => movies_collection[title_i][:title]
+
+    }
 
     title_i += 1
 
